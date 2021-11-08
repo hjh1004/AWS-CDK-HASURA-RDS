dcd4ebd85f92f5eda93222986d4bcfa263aec45f
JUN HEO <hjh1004@gmail.com>
2021-11-08 18:56:28 +0900

    Initial commit

---
+++ lib/aws-cdk-ecs-hasura-rds-stack.ts
@@ -0,0 +1,9 @@
+import * as cdk from '@aws-cdk/core';
+
+export class AwsCdkEcsHasuraRdsStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
